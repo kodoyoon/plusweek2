a92f79dcd91664b8230a68032c6dc6b700c6a439
doyoon <doyoun0527@naver.com>
2023-12-21 15:43:57 +0900

    init commit

---
+++ src/main/java/org/example/plus2/Plus2Application.java
@@ -0,0 +1,13 @@
+package org.example.plus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plus2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Plus2Application.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/plus2/Plus2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.plus2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plus2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
